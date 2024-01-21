eadd52658f61b4de7bb56ec947557d12bbeab792
mpytlyk <mishapytlyk@gmail.com>
2024-01-21 19:49:11 +0100

    change some styles

--- src/components/ArchivModal/ArchivModal.jsx
+++ src/components/ArchivModal/ArchivModal.jsx
@@ -8,7 +8,7 @@ export default function ArchivModal() {
     <div className="archivModal" >
       <div className="confirmation">
         <h2 className="confirmation__title">Ваше оголошення додано до архіву, за потребою ви зможете його відновити. Прохання видаляти непотрібні оголошення, одночасно в архіві може бути не більше 5 - ти оголошень.</h2>
-        <div className="confirmation__btns">
+        <div className="confirmation__btn">
           <Button classBtn="btn-blue btn-same-width" text="Ок" />
         </div>
       </div>
